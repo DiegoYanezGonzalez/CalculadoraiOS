2b5725854003833909c88f6fd3f33c396796723e
diego <diego.y8@gmail.com>
2021-05-07 09:58:16 -0400

    DesignDone

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -18,6 +18,45 @@ export const CalculadoraScreen = () => {
                     
 
             </View>
+            
+            <View style={styles.fila}>
+
+                    <BotonCalc texto="7" />
+                    <BotonCalc texto="8" />
+                    <BotonCalc texto="9" />
+                    <BotonCalc texto="x" color='#FF9427'/>
+                    
+
+            </View>
+
+            <View style={styles.fila}>
+
+                    <BotonCalc texto="4" />
+                    <BotonCalc texto="5" />
+                    <BotonCalc texto="6" />
+                    <BotonCalc texto="-" color='#FF9427'/>
+                    
+
+            </View>
+
+            <View style={styles.fila}>
+
+                    <BotonCalc texto="1" />
+                    <BotonCalc texto="2" />
+                    <BotonCalc texto="3" />
+                    <BotonCalc texto="+" color='#FF9427'/>
+                    
+
+            </View>
+
+            <View style={styles.fila}>
+
+                    <BotonCalc texto="0" ancho />
+                    <BotonCalc texto="." />
+                    <BotonCalc texto="=" color='#FF9427'/>
+                    
+
+            </View>
         </View>
         
     )
