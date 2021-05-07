2894d473e5682677e68efd08ce8ff5fd685eab01
diego <diego.y8@gmail.com>
2021-05-07 18:50:20 -0400

    BottonOperationMathematicsInit

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
-import { Text, View } from 'react-native'
-import { BotonCalc } from '../components/BotonCalc'
-import { styles } from '../theme/appTheme'
+import React, { useRef, useState } from 'react';
+import { Text, View } from 'react-native';
+import { BotonCalc } from '../components/BotonCalc';
+import { styles } from '../theme/appTheme';
+
+enum Operadores{
+        sumar,
+        restar,
+        multiplicar,
+        dividir
+}
 
 export const CalculadoraScreen = () => {
 
-        const [numeroAnterior, setNumeroAnterior] = useState('0');
+const [numeroAnterior, setNumeroAnterior] = useState('0');
 const [numero, setNumero] = useState('0');
 
+const ultimaOperacion = useRef<Operadores>();
 
 const limpiar = () => {
      setNumero('0');
+     setNumeroAnterior('0');
 }
 
 const armarNumero = (numeroTexto:string) => {
@@ -42,6 +51,31 @@ const armarNumero = (numeroTexto:string) => {
 
    }
 
+   const btnDelete = () =>{
+           let negativo='';
+           let numeroTemp = numero;
+           if(numero.includes('-')){
+                   negativo='-';
+                   numeroTemp=numero.substr(1);
+           }
+           if (numero.length>1){
+                   setNumero(negativo+numeroTemp.slice(0,-1));
+           }else{
+                   setNumero('0');
+           }
+
+   }
+
+   const cambiarNumPorAnterior = () =>{
+           if(numero.endsWith('.')){
+                setNumeroAnterior(numero.slice(0,-1));
+           }else{
+                   setNumeroAnterior(numero);
+           }
+     
+           setNumero('0');
+   }
+
    const positivoNegativo = () => {
            if (numero.includes('-') ) {
                    setNumero(numero.replace('-' , ''));
@@ -49,11 +83,32 @@ const armarNumero = (numeroTexto:string) => {
                 setNumero('-' + numero);
 
            }
-   }
+        }
+
+        const btnDividir = () =>{
+                cambiarNumPorAnterior();
+                ultimaOperacion.current =Operadores.dividir;
+        }
+        const btnMultiplicar = () =>{
+                cambiarNumPorAnterior();
+                ultimaOperacion.current =Operadores.multiplicar;
+        }
+        const btnRestar = () =>{
+                cambiarNumPorAnterior();
+                ultimaOperacion.current =Operadores.restar;
+        }
+        const btnSumar = () =>{
+                cambiarNumPorAnterior();
+                ultimaOperacion.current =Operadores.sumar;
+        }
 
     return (
         <View style={styles.calculadoraContainer}>
-            <Text style={styles.resultadoPequeno}> {numeroAnterior} </Text>
+                {
+                        (numeroAnterior === '0') && 
+                        (<Text style={styles.resultadoPequeno}> {numeroAnterior} </Text>)
+                }
+           
             <Text 
             style={styles.resultado}
             numberOfLines={1}
@@ -66,8 +121,8 @@ const armarNumero = (numeroTexto:string) => {
 
                     <BotonCalc texto="C" color='#9B9B9B' accion={limpiar} />
                     <BotonCalc texto="+/-" color='#9B9B9B'accion={positivoNegativo}/>
-                    <BotonCalc texto="del" color='#9B9B9B'accion={limpiar}/>
-                    <BotonCalc texto="/" color='#FF9427'accion={limpiar}/>
+                    <BotonCalc texto="del" color='#9B9B9B'accion={btnDelete}/>
+                    <BotonCalc texto="/" color='#FF9427'accion={btnDividir}/>
                     
 
             </View>
@@ -77,7 +132,7 @@ const armarNumero = (numeroTexto:string) => {
                     <BotonCalc texto="7" accion= {armarNumero} />
                     <BotonCalc texto="8" accion= {armarNumero}/>
                     <BotonCalc texto="9" accion= {armarNumero}/>
-                    <BotonCalc texto="x" color='#FF9427'accion={limpiar}/>
+                    <BotonCalc texto="x" color='#FF9427'accion={btnMultiplicar}/>
                     
 
             </View>
@@ -87,7 +142,7 @@ const armarNumero = (numeroTexto:string) => {
                     <BotonCalc texto="4" accion= {armarNumero}/>
                     <BotonCalc texto="5" accion= {armarNumero}/>
                     <BotonCalc texto="6" accion= {armarNumero}/>
-                    <BotonCalc texto="-" color='#FF9427'accion={limpiar}/>
+                    <BotonCalc texto="-" color='#FF9427'accion={btnRestar}/>
                     
 
             </View>
@@ -97,7 +152,7 @@ const armarNumero = (numeroTexto:string) => {
                     <BotonCalc texto="1" accion= {armarNumero}/>
                     <BotonCalc texto="2" accion= {armarNumero}/>
                     <BotonCalc texto="3" accion= {armarNumero}/>
-                    <BotonCalc texto="+" color='#FF9427'accion={limpiar}/>
+                    <BotonCalc texto="+" color='#FF9427'accion={btnSumar}/>
                     
 
             </View>
